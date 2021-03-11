0607286510178e3b1d3e7055ca635bbfc679707b
Peter Levinsky <pele@easj.dk>
2021-03-11 11:24:09 +0100

    Kommentare indsat

--- ClassDemoHotelDB/DBWorker.cs
+++ ClassDemoHotelDB/DBWorker.cs
@@ -9,6 +9,7 @@ namespace ClassDemoHotelDB
 {
     public class DBWorker
     {
+        // findes i SQL server objekt browser -> database -> properties -> ConnectionString 
         private const String ConnectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=HotelDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         public DBWorker()
         {
@@ -34,19 +35,27 @@ namespace ClassDemoHotelDB
 
         }
 
+
+        private const string GetAllSQL = "select * from Guest";
         public List<Guest> GetAllGuests()
         {
             List<Guest> liste = new List<Guest>();
 
+            // opretter en forbindelse til databasen (lokalt eller i skyen afhængig af connectionstring)
             using (SqlConnection conn = new SqlConnection(ConnectionString))
             {
                 conn.Open();
 
-                using (SqlCommand sql = new SqlCommand("select * from Guest", conn))
+                // laver en SQL kommando som kan sendes til databasen
+                using (SqlCommand sql = new SqlCommand(GetAllSQL, conn))
                 {
+                    // her sendes sql kommando til database og der kommer et svar
                     SqlDataReader reader = sql.ExecuteReader();
+
+                    // læser samtlige rækker i svaret fra databasen
                     while (reader.Read())
                     {
+                        // mapper een række til eet objekt
                         Guest g = MakeGuest(reader);
                         liste.Add(g);
                     }
@@ -57,14 +66,17 @@ namespace ClassDemoHotelDB
         }
 
 
+        // sql request med en parameter (@ID) til at indsætte et id 
+        private const string GetGuestByIdSQL = "select * from Guest where Guest_No = @ID";
         public Guest GetGuestsbyId(int id)
         {
             using (SqlConnection conn = new SqlConnection(ConnectionString))
             {
                 conn.Open();
 
-                using (SqlCommand sql = new SqlCommand("select * from Guest where Guest_No = @ID", conn))
+                using (SqlCommand sql = new SqlCommand(GetGuestByIdSQL, conn))
                 {
+                    // giver parameteren @ID en konkret værdi id
                     sql.Parameters.AddWithValue("@ID", id);
 
                     SqlDataReader reader = sql.ExecuteReader();
@@ -92,6 +104,8 @@ namespace ClassDemoHotelDB
                 {
                     sql.Parameters.AddWithValue("@ID", id);
 
+                    // her sendes sql kommando (insert,delete eller update) til databasen
+                    // svaret er antal rækker der er berørt af denne sql request fx. 0 = ingen slettet 1 = en er slettet
                     int rowsAffected = sql.ExecuteNonQuery();
 
                     if (rowsAffected == 1)
@@ -103,14 +117,23 @@ namespace ClassDemoHotelDB
             throw new KeyNotFoundException("Der var ingen med id = " + id);
         }
 
+
+
+        // mapper een række til eet objekt
         private Guest MakeGuest(SqlDataReader reader)
         {
             Guest g = new Guest();
 
+            /*
+             * version 1
+             */
             //g.Id = Convert.ToInt32(reader["Guest_No"]);
             //g.Name = Convert.ToString(reader["Name"]);
             //g.Address = Convert.ToString(reader["Address"]);
 
+            /*
+             * version 2
+             */
             g.Id = reader.GetInt32(0);
             g.Name = reader.GetString(1);
             g.Address = reader.GetString(2);
